8a76332b158e3a273bfc541c688eba2db928f015
Anshulpostman <114227437+Anshulpostman@users.noreply.github.com>
2024-12-05 18:09:28 +0530

    Update into.sql

--- into.sql
+++ into.sql
@@ -1,1 +1,9 @@
 --select Territoryid INTO newregionid2017 from Territories
+
+--select Territoryid INTO newregionid2017 from Territories
+
+
+--The following SQL statement copies data from more than one table into a new table:
+
+Select Orders.ShipName, Orders.ShipCity, customers.City, customers.Country INTO Newtable2024 
+from Orders left join Customers on Orders.CustomerID= customers.CustomerID
